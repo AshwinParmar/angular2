94baed79365a7b8133b8719ba1e3e08d1ac77bcf
Ashwin Parmar <ashwin.d.parmar@accenture.com>
2017-07-27 06:39:22 +0000

    node_modules added, and component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  //template: `<h1>Hello {{name}}</h1><div>To Tutorials Point</div>`,
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { 
+  appTitle: string = 'Welcome';
+  name: string = 'Ashwin';
+  appStatus: boolean = true;
+  appList: any[] = [{
+    "ID": "1",
+    "Name": "One",
+    "url": 'app/images/one.png'
+  },
+  {
+    "ID": "2",
+    "Name": "Two",
+    "url": 'app/images/two.png'
+  }];
+}
